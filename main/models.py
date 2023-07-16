a76f6015bdcbf2c22983bb9c918a6181dbeb3d7c
Eisiehame <knurov24@gmail.com>
2023-07-16 20:52:51 +0200

    lesson+{

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='main/', verbose_name='изображение', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+        ordering = ('name',)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='main/', verbose_name='изображение', **NULLABLE)
+    url = models.CharField(verbose_name='ссылка на видео')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+        ordering = ('name',)
+
--- main/views.py
+++ main/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views import generic
+from main.models import Lesson
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+
+class LessonListView(generic.ListView):
+    model = Lesson
+
+
+class LessonDetailView(generic.DetailView):
+    model = Lesson
+
+
+class LessonCreateView(generic.CreateView):
+    model = Lesson
+    success_url = reverse_lazy('main:lesson_list')
+    fields = ('name', 'description', 'preview', 'url')
+
+
+class LessonDeleteView(generic.DeleteView):
+    model = Lesson
+    success_url = reverse_lazy('main:lesson_list')
+
+
+class LessonUpdateView(generic.UpdateView):
+    model = Lesson
+    success_url = reverse_lazy('main:lesson_list')
+    fields = ('name', 'description', 'preview', 'url')
