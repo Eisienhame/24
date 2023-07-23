f1fb728b16c54fc243a98f6c9f754f926252460a
Eisiehame <knurov24@gmail.com>
2023-07-23 19:13:17 +0200

    5/6 миграции + документация

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'django_filters',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
