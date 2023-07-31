bd70f38db62d960a22997147bd19f96a701319ef
Eisiehame <knurov24@gmail.com>
2023-07-31 21:47:19 +0200

    6/6 task+

--- main/tasks.py
+++ main/tasks.py
@@ -1,16 +1,28 @@
 from celery import shared_task
 from django.core.mail import send_mail
-
+from datetime import datetime, timedelta
 from config import settings
 from main.models import SubscriptionCourse
+from users.models import User
 
 
 @shared_task
 def send_updated_email(course):
-    subscribers = SubscriptionCourse.objects.get(course=course)
-    send_mail(
-        'ALARM!',
-        f'Сообщаем, что курс на который вы подписаны  обновлён',
-        settings.EMAIL_HOST_USER,
-        [subscribers.user]
-    )
+    subscribers_list = SubscriptionCourse.objects.filter(course=course)
+    for sub in subscribers_list:
+        print('Отправлено сообщение об обновление')
+        send_mail(
+            'ALARM!',
+            f'Сообщаем, что курс на который вы подписаны  обновлён',
+            settings.EMAIL_HOST_USER,
+            [sub.user]
+        )
+
+@shared_task
+def check_user():
+
+    now_date = datetime.now()
+    one_month_ago = now_date - timedelta(days=30)
+    inactive_users = User.objects.filter(last_login__lt=one_month_ago)
+    inactive_users.update(is_active=False)
+    print(inactive_users)
